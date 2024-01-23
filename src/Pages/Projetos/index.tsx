6e591de2dbe8bfb6f6e8b3ab38e13b5b516596f6
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-01-22 21:12:45 -0300

    Atualização

---
+++ src/Pages/Projetos/index.tsx
@@ -0,0 +1,20 @@
+import Footer from "../../Components/Footer/Footer"
+import Header from "../../Components/Header/Header"
+import "./style.sass"
+
+
+export default function Projetos(){
+    return(
+        <main>
+            <Header/>
+
+
+
+
+
+
+
+            <Footer/>
+        </main>
+    )
+}
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home';
 import About from "../Pages/Sobre"
+import Projetos from '../Pages/Projetos';
 
 
 function RoutesApp() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="sobre-mim" element={<About/>} />
+      <Route path="/" element={<Home />}/>
+      <Route path="sobre-mim" element={<About/>}/>
+      <Route path="Projetos" element={<Projetos/>}/>
     </Routes>
   );
 }
